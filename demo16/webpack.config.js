5eaad352d16ffabd41bd644c65e91957cddb8c36
yvan <57882634@qq.com>
2018-08-05 19:31:00 +0800

    finished

--- demo16/webpack.config.js
+++ demo16/webpack.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  entry: './index.js',
+  entry: './index.jsx',
   output: {
     filename: 'bundle.js'
   },
   module: {
     loaders:[
       { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.js[x]?$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=react' },
+      { test: /\.js[x]?$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' },
     ]
   }
 };
