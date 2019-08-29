f41b81ea02e445440f17f3d75a866f87d1f19949
yvan <57882634@qq.com>
2019-08-29 10:15:19 +0800

    first commit

--- demo15/index.js
+++ demo15/index.js
@@ -3,3 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if (module.hot) {
+  console.log(module.hot.status());
+  module.hot.accept('./App.js', function (params) {
+    console.log('Accepting the updated App module!');
+  })
+}
